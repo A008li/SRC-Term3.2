dc4408a60960eae7ba39fad7043270f5fc2c2d90
A008li <alifar008afr@gmail.com>
2023-07-18 12:02:51 +0330

    session3

---
+++ session3/charcterContorls.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    //printf("Full name:\tAli Farzaneh\nAge:\t18\nGrade:\tSRC-Term3");
+
+    printf("Full name:\tAli Farzaneh");
+    printf("\nAge:\t\t18");
+    printf("\nGrade:\t\tSRC-Term3");
+
+    return 0;
+}
---
+++ session3/printName.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(){
+    printf("Ali Farzaneh");
+
+    return 0;
+}
