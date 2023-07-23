ddca6556914ce3080d45f3ce50344247aefd019a
A008li <alifar008afr@gmail.com>
2023-07-23 12:07:15 +0330

    session4

---
+++ session4/characterControls.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int number1 = 10;
+    int number2 = 1567;
+
+    printf("number1 = %d\tnumber2 = %d", number1, number2); 
+
+    return 0;
+}
---
+++ session4/scanf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int num;
+
+    printf("Please enter a intger number:\t");
+    scanf("%d", &num);
+
+    printf("\n\nyour number is: %d", num);
+
+    return 0;
+}
