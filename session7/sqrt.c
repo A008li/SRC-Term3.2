9de3e9eaeef0a05ad78fdc59e3c05e655b84a293
A008li <alifar008afr@gmail.com>
2023-07-30 13:21:38 +0330

    session7

---
+++ session7/sqrt.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    float sq;
+
+    printf("enter any value:\t");
+    scanf("%f", &sq);
+
+    printf("sqrt = %1.3f", sqrt(sq));
+
+    return 0;
+}
